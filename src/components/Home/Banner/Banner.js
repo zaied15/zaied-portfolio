f2e2d6d1703cc514139c8817224d1ddefd75b083
unknown <mda800@gmail.com>
2022-06-09 13:06:10 +0600

    first commit

---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import zaied from "../../../images/zaied.jpeg";
+
+const Banner = () => {
+  let TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtType.prototype.tick = function () {
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span className="wrap">' + this.txt + "</span>";
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function () {
+    let elements = document.getElementsByClassName("typewrite");
+    for (let i = 0; i < elements.length; i++) {
+      let toRotate = elements[i].getAttribute("data-type");
+      let period = elements[i].getAttribute("data-period");
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    let css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+  };
+  return (
+    <div className="hero min-h-screen bg-base-100">
+      <div className="hero-content flex-col lg:flex-row lg:justify-between">
+        <img src={zaied} className="max-w-lg rounded-lg shadow-2xl" alt="" />
+        <div>
+          <p>Hello, I'm</p>
+          <h1 className="text-5xl font-bold">Md. Anwar Zaied</h1>
+          <h1 className="h-7">
+            <a
+              to=""
+              className="typewrite text-xl"
+              data-period="2000"
+              data-type='[ "Full Stack Developer (MERN).", "I love to write code and develope application." ]'
+            >
+              <span className="wrap"></span>
+            </a>
+          </h1>
+          <button className="btn btn-neutral mt-5">Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6A98F0",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+};
